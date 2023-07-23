937638dd9a030bd0adb8375302bd032c2b904fcb
Moonflakes <33527879+Moonflakes@users.noreply.github.com>
2023-07-23 12:58:07 +0200

    menu animation

---
+++ Art Spotter copy/main.js
@@ -0,0 +1,20 @@
+document
+  .getElementById("click-menu")
+  .addEventListener("click", () => controlMenuAnimation("open"));
+
+document
+  .getElementById("click-close-menu")
+  .addEventListener("click", () => controlMenuAnimation("close"));
+
+function controlMenuAnimation(status) {
+  const toRemove = status === "close" ? "open-animation" : "close-animation";
+  const toAdd = status === "close" ? "close-animation" : "open-animation";
+  const menu = document.querySelector("#menu-main");
+  const background = document.querySelector("#menu-background-overlay");
+  menu.classList.remove(toRemove);
+  background.classList.remove(toRemove);
+  setTimeout(() => {
+    menu.classList.add(toAdd);
+    background.classList.add(toAdd);
+  }, 0);
+}
