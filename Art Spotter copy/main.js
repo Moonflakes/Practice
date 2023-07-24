79466cd94164e7d2afec94046158a60fb58c4ed6
Moonflakes <33527879+Moonflakes@users.noreply.github.com>
2023-07-24 19:27:56 +0200

    about part

--- Art Spotter copy/main.js
+++ Art Spotter copy/main.js
@@ -6,9 +6,30 @@ document
   .getElementById("click-close-menu")
   .addEventListener("click", () => controlMenuAnimation("close"));
 
+// document
+//   .getElementById("find-me")
+//   .addEventListener("mouseleave", () =>
+//     controlFindButtonAnimation("mouseleave")
+//   );
+// document
+//   .getElementById("find-me")
+//   .addEventListener("mouseenter", () =>
+//     controlFindButtonAnimation("mouseenter")
+//   );
+
 function controlMenuAnimation(status) {
-  const toRemove = status === "close" ? "open-animation" : "close-animation";
-  const toAdd = status === "close" ? "close-animation" : "open-animation";
+  let toRemove = "";
+  let toAdd = "";
+  switch (status) {
+    case "close":
+      toRemove = "open-animation";
+      toAdd = "close-animation";
+      break;
+    case "open":
+      toRemove = "close-animation";
+      toAdd = "open-animation";
+      break;
+  }
   const menu = document.querySelector("#menu-main");
   const background = document.querySelector("#menu-overlay");
   menu.classList.remove(toRemove);
@@ -18,3 +39,26 @@ function controlMenuAnimation(status) {
     background.classList.add(toAdd);
   }, 0);
 }
+
+// function controlFindButtonAnimation(status) {
+//   let toRemove = "";
+//   let toAdd = "";
+//   switch (status) {
+//     case "mouseleave":
+//       toRemove = "mouseenter-animation";
+//       toAdd = "mouseleave-animation";
+//       break;
+//     case "mouseenter":
+//       toRemove = "mouseleave-animation";
+//       toAdd = "mouseenter-animation";
+//       break;
+//   }
+//   const text = document.querySelector("#find-me-text");
+//   const svg = document.querySelector("#find-me-svg");
+//   text.classList.remove(toRemove);
+//   svg.classList.remove(toRemove);
+//   setTimeout(() => {
+//     text.classList.add(toAdd);
+//     svg.classList.add(toAdd);
+//   }, 0);
+// }
