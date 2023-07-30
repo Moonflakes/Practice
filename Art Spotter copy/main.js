d271cfde5772f3fd3ab69d04f18e160919dd6e91
Moonflakes <33527879+Moonflakes@users.noreply.github.com>
2023-07-30 19:58:24 +0200

    refacto and add card button animation

--- Art Spotter copy/main.js
+++ Art Spotter copy/main.js
@@ -1,51 +1,92 @@
+const controlMenuAnimation = () => {
+  const menu = document.querySelector(".menu-main");
+  const background = document.querySelector(".menu-overlay");
+  ["close", "open"].map((status) => {
+    // remove or add classes status for css transition activation
+    menu.classList.toggle(`menu-main-${status}`);
+    background.classList.toggle(`menu-overlay-${status}`);
+  });
+};
+
+const animateElement = (element, fromValue, toValue, options) => {
+  element.animate([{ transform: fromValue }, { transform: toValue }], options);
+};
+
+const controlEnterLeaveArrowAnimation = (animationProps) => {
+  animationProps.map((props) => {
+    const { element, targetPosition, animationOptions } = props;
+    // get current positions
+    // initial position or position during animation
+    const currentElemPosition = window.getComputedStyle(element).transform;
+    animateElement(
+      element,
+      currentElemPosition,
+      targetPosition,
+      animationOptions
+    );
+  });
+};
+
 const menuOpenButton = document.querySelector(".open-menu-button");
 const menuCloseButton = document.querySelector(".close-menu-button");
 
 menuOpenButton.addEventListener("click", controlMenuAnimation);
 menuCloseButton.addEventListener("click", controlMenuAnimation);
 
-function controlMenuAnimation() {
-  const menu = document.querySelector(".menu-main");
-  const background = document.querySelector(".menu-overlay");
-  ["close", "open"].map((status) => {
-    // remove or add classes status
-    menu.classList.toggle(`menu-main-${status}`);
-    background.classList.toggle(`menu-overlay-${status}`);
-  });
-}
-
-const findMeButton = document.querySelector(".button-find-me");
-const text = document.querySelector(".find-me-text");
-const svg = document.querySelector(".arrow-svg");
-
-findMeButton.addEventListener("mouseenter", controlFindMeButtonAnimation);
-findMeButton.addEventListener("mouseleave", controlFindMeButtonAnimation);
-
-function controlFindMeButtonAnimation(mouseEvent) {
-  const eventType = mouseEvent.type;
-
-  // get current positions
-  // initial position or position during animation
-  const currentSvgPosition = window.getComputedStyle(svg).transform;
-  const currentTextPosition = window.getComputedStyle(text).transform;
-
-  // set target positions according to the eventType mouseenter or mouseleave
-  let targetSvgPosition = "";
-  let targetTextPosition = "";
-  switch (eventType) {
-    case "mouseenter":
-      targetSvgPosition = "translateX(0px) translateY(0px) rotate(-45deg)";
-      targetTextPosition = "translateX(-30px)";
-      break;
-    case "mouseleave":
-      targetSvgPosition = "translateX(100px) translateY(-100px)";
-      targetTextPosition = "translateX(0px)";
-      break;
-  }
-  animateElement(text, currentTextPosition, targetTextPosition, { duration: 400, fill: "forwards" });
-  animateElement(svg, currentSvgPosition, targetSvgPosition, { duration: 400, fill: "forwards" });
-}
-
-const animateElement = (element, fromValue, toValue, options) => {
-  element.animate([{ transform: fromValue }, { transform: toValue }], options);
+const mouseEvents = ["mouseenter", "mouseleave"];
+const buttons = document.querySelectorAll(".button");
+const animationOptions = {
+  duration: 400,
+  easing: "ease",
+  fill: "forwards",
 };
+const buttonSvgTargetPositions = {
+  mouseenter: "translateX(0px) translateY(0px) rotate(-45deg)",
+  mouseleave: "translateX(100px) translateY(-100px)",
+};
+const buttonTextTargetPositions = {
+  mouseenter: "translateX(-30px)",
+  mouseleave: "translateX(0px)",
+};
+
+Array.from(buttons).map((button) => {
+  const textButton = button.firstElementChild;
+  const arrowSvgButton = button.lastElementChild;
+  mouseEvents.map((mouseEvent) =>
+    button.addEventListener(mouseEvent, () =>
+      controlEnterLeaveArrowAnimation([
+        {
+          element: textButton,
+          targetPosition: buttonTextTargetPositions[mouseEvent],
+          animationOptions,
+        },
+        {
+          element: arrowSvgButton,
+          targetPosition: buttonSvgTargetPositions[mouseEvent],
+          animationOptions,
+        },
+      ])
+    )
+  );
+});
+
+const cardButtons = document.querySelectorAll(".card-button");
+const cardSvgTargetPositions = {
+  mouseenter: "translateX(156px) translateY(-228px) rotate(-45deg)",
+  mouseleave: "translateX(338px) translateY(-338px)",
+};
+
+Array.from(cardButtons).map((cardButton) => {
+  const arrowCardButton = cardButton.firstElementChild.lastElementChild;
+  mouseEvents.map((mouseEvent) =>
+    cardButton.addEventListener(mouseEvent, () =>
+      controlEnterLeaveArrowAnimation([
+        {
+          element: arrowCardButton,
+          targetPosition: cardSvgTargetPositions[mouseEvent],
+          animationOptions,
+        },
+      ])
+    )
+  );
+});
